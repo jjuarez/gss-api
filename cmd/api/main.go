f490b84d71461d8c52243b602b679a48fb890b82
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-09-26 19:05:05 +0200

    feat: Back to Echo, minimal entrypoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,58 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
-	"time"
 
 	config "github.com/jjuarez/gss-api/internal/config"
 	utils "github.com/jjuarez/gss-api/internal/utils"
-	"github.com/rs/zerolog"
+	"github.com/labstack/echo/v4"
 )
 
 var (
 	// Version the version of the GSS API release
 	Version string
-	// GitCommit information about the CVS
-	GitCommit string
 )
-
-type application struct {
-	cfg    config.Config
-	logger zerolog.Logger
-}
 
 func main() {
 	environment := utils.Getenv(config.GSSAPIEnvironmentEnvKey, config.DefaultEnvironment)
 	config.SetupEnvironment(environment)
 
-	logger := zerolog.New(os.Stdout).
-		Level(zerolog.TraceLevel).
-		With().
-		Timestamp().
-		Caller().
-		Logger()
-
 	srvCfg, err := config.NewConfig()
 	if err != nil {
-		logger.Error().Msgf("Error: %v", err)
+		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	app := &application{
-		cfg:    *srvCfg,
-		logger: logger,
-	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 
-	srv := &http.Server{
-		Addr:         app.cfg.Address(),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
+	e.GET("/v1/api/healthcheck", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Super healthy")
+	})
 
-	logger.Debug().Msgf("Starting %v server (%s) listening on -> %s", app.cfg.Environment, Version, srv.Addr)
-	err = srv.ListenAndServe()
-	logger.Error().Msgf("Error: %v", err)
+	e.Logger.Fatal(e.Start(srvCfg.Address()))
+	e.Logger.Info("Starting %v server (%s) listening on -> %s\n", srvCfg.Environment, Version, srvCfg.Address)
 }
